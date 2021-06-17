3eda0b601e3f3ae750d89d2e071e41a5c5ca90e9
Risoaldo Nóbrega <noobregarisoaldo@gmail.com>
2021-06-16 22:48:05 -0300

    create crud with nest, docker and postgres, i'm use resource for create the crud

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'docker',
+      database: 'users',
+      autoLoadEntities: true,
+      synchronize: true
+    }),
+    UsersModule
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
---
+++ src/users/users.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) { }
+  create(createUserDto: CreateUserDto) {
+    const user = this.usersRepository.save(createUserDto)
+    return user
+  }
+
+  findAll() {
+    return this.usersRepository.find();
+  }
+
+  findOne(id: string): Promise<User> {
+    return this.usersRepository.findOne(id);
+  }
+
+  update(id: string, updateUserDto: UpdateUserDto) {
+    return this.usersRepository.update(id, updateUserDto);
+  }
+
+  async remove(id: string): Promise<void> {
+   await this.usersRepository.delete(id);
+  }
+}
